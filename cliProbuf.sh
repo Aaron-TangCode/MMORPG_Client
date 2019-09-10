34446dc7407bc53c61f32c12a470ce14e66661c2
TangHaiLin <1025741620@qq.com>
2019-09-10 11:59:26 +0800

    阶段六-项目上线v1.8.4-添加脚本文件

---
+++ cliProbuf.sh
@@ -0,0 +1,20 @@
+protoc --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\Role.proto
+protoc --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\Goods.proto
+protoc --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\Map.proto
+protoc --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\Npc.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgAuctionInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgBossInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgChatInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgEmailInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgEquipInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgGangInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgGoodsInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgMapInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgNpcInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgRankInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgRoleInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgShopInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgSkillInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgTaskInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgTradeInfo.proto
+protoc --proto_path=src\main\java\com\hailintang\client\protobuf --java_out=src\main\java src\main\java\com\hailintang\client\protobuf\MsgUserInfo.proto
